4d8b598378726f4d8fa16cf1e08c742330e06492
Cameron Bieganek <8310743+CameronBieganek@users.noreply.github.com>
2023-08-05 09:55:27 -0500

    Add default implementations for `add_vertices!` and `add_edges!`.

--- src/GraphInterface.jl
+++ src/GraphInterface.jl
@@ -9,6 +9,18 @@ function neighbors end
 
 function add_vertex! end
 
+function add_vertices!(g, vs)
+    for v in vs
+        add_vertex!(g, v)
+    end
+end
+
 function add_edge! end
+
+function add_edges!(g, es)
+    for e in es
+        add_edge!(g, e)
+    end
+end
 
 end
